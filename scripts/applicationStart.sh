f31b97ee8984c784bea7c378d3bcacfecb329954
eojhet <eojhet@pop-os.localdomain>
2023-10-14 12:31:42 -0400

    Tweaked PM2, NGINX, Site title and desc

--- scripts/applicationStart.sh
+++ scripts/applicationStart.sh
@@ -2,7 +2,9 @@
 
 cd /home/ubuntu/joeygonz
 npm run build
+pm2 delete "joeygonz"
+pm2 save --force
 pm2 start npm --name "joeygonz" -- start
 pm2 startup
-pm2 save
+pm2 save --force
 pm2 restart all
