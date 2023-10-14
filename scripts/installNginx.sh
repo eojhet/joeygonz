e73cde72631e479116f7c54d85c2f7e5503900a3
eojhet <eojhet@pop-os.localdomain>
2023-10-13 22:07:30 -0400

    appspec.yml

--- scripts/installNginx.sh
+++ scripts/installNginx.sh
@@ -2,7 +2,8 @@
 
 apt-get update
 apt-get install nginx -y
-service nginx start
-cp /home/ubuntu/joeygonz/joegonz.com /etc/nginx/sites-available
+systemctl enable nginx
+systemctl start nginx
+cp /home/ubuntu/joeygonz/proxy/joeygonz.com /etc/nginx/sites-available
 ln -s /etc/nginx/sites-available/joeygonz.com /etc/nginx/sites-enabled
-service nginx restart
+systemctl restart nginx
