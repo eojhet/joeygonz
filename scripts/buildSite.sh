dfbd5663d55567113dcdce03f9b9330cd0cfcc3f
eojhet <eojhet@pop-os.localdomain>
2023-10-15 13:26:22 -0400

    setup after custom AMI executes

--- scripts/buildSite.sh
+++ scripts/buildSite.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+cp /home/ubuntu/joeygonz/proxy/joeygonz.com /etc/nginx/sites-available
+ln -s /etc/nginx/sites-available/joeygonz.com /etc/nginx/sites-enabled
+systemctl restart nginx
+
 cd /home/ubuntu/joeygonz
 npm install
 npm install pm2 -g
