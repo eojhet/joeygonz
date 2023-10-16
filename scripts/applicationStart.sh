8e7754ab34d4f66d685f63e9d26ea3342fbb815e
eojhet <eojhet@gmail.com>
2023-10-16 14:27:31 -0400

    switch to user via heredoc

--- scripts/applicationStart.sh
+++ scripts/applicationStart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo switching to user for site startup
-sudo su ubuntu
+sudo -i -u ubuntu bash << EOF
 sudo chown -R ubuntu /home/ubuntu/joeygonz/
 cd /home/ubuntu/joeygonz
 npm install
@@ -14,4 +14,4 @@ pm2 startup
 sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu
 pm2 save --force
 pm2 restart all
-exit
+EOF
