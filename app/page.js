331b7b19af4f35b399cbd4e9a7ed61b3970c289a
eojhet <eojhet@pop-os.localdomain>
2023-10-22 14:51:43 -0400

    reach me

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.joeygonz}>
         <h1>JoeyGonz</h1>
-        <p><i>I can save you.</i></p>
+        <p><i><a href="mailto:joe@joeygonz.com">Reach Me</a></i></p>
       </div>
       <div className={styles.card}>
         <h2>In the meantime, go and make a boring log at my other site {"->"} <a href="https://boring.eojhet.com">Boring.Eojhet.com</a></h2>
