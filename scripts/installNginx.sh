641c35fc0c94e6b54e59a5169411344246f86816
eojhet <eojhet@pop-os.localdomain>
2023-10-22 13:39:09 -0400

    cleaned up scripts

--- scripts/installNginx.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-apt-get update
-apt-get install nginx -y
-systemctl enable nginx
-systemctl start nginx
-cp /home/ubuntu/joeygonz/proxy/joeygonz.com /etc/nginx/sites-available
-ln -s /etc/nginx/sites-available/joeygonz.com /etc/nginx/sites-enabled
-systemctl restart nginx
